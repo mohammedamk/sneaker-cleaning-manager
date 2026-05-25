0f718f4d9c403ed18435a350f0ca94b646216281
mohammedamk <mohammedamk1@gmail.com>
2026-05-25 17:09:56 +0530

    fixed issue with the extension UI getting affected by store theme css and proxy issue

--- sneaker-cleaning-ext/src/utils/global.js
+++ sneaker-cleaning-ext/src/utils/global.js
@@ -1,5 +1,5 @@
 // development
-export const PROXY_SUB_PATH = 'sneaker-cleaning-dev';
+// export const PROXY_SUB_PATH = 'sneaker-cleaning-dev';
 
 // production
-// export const PROXY_SUB_PATH = 'sneaker-cleaning';
+export const PROXY_SUB_PATH = 'sneaker-cleaning';
